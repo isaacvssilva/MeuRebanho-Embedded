e7da6e7ec91889a8b90cfadfe9aadb7c89e0ac1d
isaac <isaacvinicius2121@alu.ufc.br>
2023-12-08 13:36:35 -0300

    update - Inc. wifi manager

--- meuRabanho-gateway/src/main.cpp
+++ meuRabanho-gateway/src/main.cpp
@@ -16,23 +16,24 @@
  *
  * =====================================================================================
  */
-
-#include <WiFi.h>
+#include "Arduino.h"
+#include <WiFiManager.h>
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-#define WIFI_SSID "admin"
-#define WIFI_PASSWORD "admin"
 #define API_KEY ""      // firebase cloud
 #define DATABASE_URL "" // firebase cloud
 
 #include <SPI.h>
 #include <LoRa.h>
-
+#define LED_WIFI_ON 26 
+#define LED_WIFI_OFF 27
+#define LED_LORA_ON 16
+#define LED_LORA_OFF 17
 // Definindo as portas usadas pelo modulo receptor
 #define ss 5    // Definindo o pino SS (Selecao de Escravo)
-#define rst 14  // Definindo o pino de reset
+#define rst 4  // Definindo o pino de reset
 #define dio0 2   // Definindo o pino DIO0 (Interrupcao de recepcao)
 
 FirebaseData fdbo;
@@ -41,6 +42,59 @@ FirebaseConfig config;
 
 unsigned long sendDataPrevMilis = 0;
 bool signupOk = false;
+
+void lora_init(void) {
+  pinMode(LED_LORA_ON, OUTPUT);
+  pinMode(LED_LORA_OFF, OUTPUT);
+  while (!Serial);
+  Serial.println("Iniciando o LoRa Receiver");
+
+  /* Configurando o modulo transceptor LoRa */
+  LoRa.setPins(ss, rst, dio0);
+  digitalWrite(LED_LORA_OFF, HIGH);
+
+  /* Substituindo o argumento LoRa.begin(---E-) com a frequência da sua região */
+  /* 433E6 para Ásia */
+  /* 866E6 para Europa */
+  /* 915E6 para América do Norte */
+  while (!LoRa.begin(915E6)) {}
+
+  /* Definindo a palavra de sincronização (sync word) para corresponder ao transmissor */
+  /* A palavra de sincronização garante que você não receba mensagens LoRa de outros transceptores LoRa */
+  /* Variando de 0 a 0xFF */
+  LoRa.setSyncWord(0xF3);
+  digitalWrite(LED_LORA_OFF, LOW);
+  digitalWrite(LED_LORA_ON, HIGH);
+  Serial.println("LoRa Inicializado com Sucesso!");
+}
+
+void wifi_config(void) {
+  /* Configurando os pinos dos LEDs como saídas */
+  pinMode(LED_WIFI_ON, OUTPUT);
+  pinMode(LED_WIFI_OFF, OUTPUT);
+  digitalWrite(LED_WIFI_OFF, HIGH);
+  /* Inicializando localmente o WiFiManager. Uma vez que ele concluir sua tarefa, não será necessário mantê-lo ativo. */
+  WiFiManager wm;
+
+  /* Redefinindo as configurações - apagando as credenciais armazenadas para testes. */
+  /* Essas credenciais estão sendo armazenadas pela biblioteca do ESP. */
+  wm.resetSettings();
+
+  bool res;
+  res = wm.autoConnect("MeuRebanho-Gateway","12345678"); /* AP protegido por senha */
+  /* Mantendo o LED de WiFi desligado (LED_WIFI_OFF) aceso */
+  if(!res) {
+    Serial.println("Falha na conexão");
+  } else {
+    /* Se você estiver chegando aqui, a conexão WiFi está sendo estabelecida */
+    Serial.println("Conectado...yeah :)");
+
+    /* Desligando o LED de WiFi desligado (LED_WIFI_OFF) */
+    digitalWrite(LED_WIFI_OFF, LOW);
+    /* Acendendo o LED de WiFi ligado (LED_WIFI_ON) */
+    digitalWrite(LED_WIFI_ON, HIGH);
+  }
+}
 
 /* Salvando coordenadas no Firebase Realtime Database */
 void saveCoordenadas(void) {
@@ -72,7 +126,7 @@ void saveCoordenadas(void) {
   delay(2000);
   
   /* Verificando se a operação de definir uma string no Firebase Realtime Database foi bem-sucedida */
-  if (Firebase.RTDB.setString(&fdbo, "Animal/GPS/Coordenadas", coordenadas)) {
+  if (Firebase.RTDB.setString(&fdbo, "Animal/marota1/GPS", coordenadas)) {
     Serial.println();
     /* Exibindo as coordenadas que foram salvas com sucesso */
     Serial.print(coordenadas);
@@ -90,62 +144,20 @@ void saveCoordenadas(void) {
 void setup() {
   Serial.begin(115200);
 
-  while (!Serial);
-  Serial.println("Iniciando o LoRa Receiver");
-
-  /* Configurando o modulo transceptor LoRa */
-  LoRa.setPins(ss, rst, dio0);
-
-  /* Substituindo o argumento LoRa.begin(---E-) com a frequência da sua região */
-  /* 433E6 para Ásia */
-  /* 866E6 para Europa */
-  /* 915E6 para América do Norte */
-  while (!LoRa.begin(915E6)) {
-    Serial.println(".");
-    delay(500);
-  }
-
-  /* Definindo a palavra de sincronização (sync word) para corresponder ao transmissor */
-  /* A palavra de sincronização garante que você não receba mensagens LoRa de outros transceptores LoRa */
-  /* Variando de 0 a 0xFF */
-  LoRa.setSyncWord(0xF3);
-  Serial.println("LoRa Inicializado com Sucesso!");
-  delay(3000);
-
-  /* Exibindo uma mensagem enquanto o dispositivo está estabelecendo uma conexão Wi-Fi */
-  Serial.println("Conectando ao Wi-Fi...");
-
-  /* Iniciando a conexão Wi-Fi com as credenciais definidas em WIFI_SSID e WIFI_PASSWORD */
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  /* Aguardando até que a conexão Wi-Fi seja estabelecida, mostrando um ponto a cada tentativa de conexão */
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    /* Aguardando por 300 milissegundos antes de verificar novamente o status da conexão */
-    delay(300);
-  }
-
-  /* Indicando o término da tentativa de conexão com uma nova linha */
-  Serial.println();
-
-  /* Exibindo uma mensagem informando que a conexão Wi-Fi foi bem-sucedida e que o IP será apresentado a seguir */
-  Serial.print("Conexão Wi-Fi estabelecida com IP: ");
-
-  /* Mostrando o endereço IP local atribuído ao dispositivo Wi-Fi */
-  Serial.println(WiFi.localIP());
-
+  wifi_config();
+  lora_init();
   /* Configurando as informações da chave de API e da URL do banco de dados do Firebase */
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
   /* Verificando se o registro no Firebase foi bem-sucedido e apresentando uma mensagem de sucesso */
   if (Firebase.signUp(&config, &auth, "", "")) {
-    Serial.println("Registrando com Sucesso");
-    signupOk = true;
+    Serial.println("SignUp com Sucesso");
+   signupOk = true;
   } else { /* Em caso de falha no registro, exibindo uma mensagem de erro com detalhes */
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
-
+  
   /* Configurando um retorno de chamada (callback) para lidar com o status do token de autenticação */
   config.token_status_callback = tokenStatusCallback;
 
@@ -154,9 +166,10 @@ void setup() {
 
   /* Solicitando a reconexão Wi-Fi ao Firebase, se necessário, permitindo a reconexão automática */
   Firebase.reconnectWiFi(true);
+  
 }
 
 void loop() {
   saveCoordenadas();
-  delay(5000);
+  delay(500);
 }
